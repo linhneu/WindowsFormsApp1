bc5f65e440d7d1e6181f33375ab1aba06de3e288
NguyenVi <nguyenvanvi2000308@gmail.com>
2021-06-06 16:12:37 +0700

    - add db

---
+++ DBSQLServerUtils.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient; 
+
+namespace WindowsFormsApp1
+{
+    // Data Source=DESKTOP-LSEGFR1\SQLEXPRESS;Initial Catalog=QUAN_LY_NHAN_SU;Integrated Security=True
+    class DBSQLServerUtils
+    {
+        public static SqlConnection GetDBConnection(string datasource, string database)
+        {
+            //
+            // Data Source=TRAN-VMWARE\SQLEXPRESS;Initial Catalog=simplehr;Persist Security Info=True;User ID=sa;Password=12345
+            //
+            string connString = @"Data Source=" + datasource + 
+                                ";Initial Catalog=" + database +
+                                ";Integrated Security=True" ;
+
+            SqlConnection conn = new SqlConnection(connString);
+            conn.Open();
+            return conn;
+        }
+
+        public static SqlConnection GetDBConnection()
+        {
+            //
+            // Data Source=TRAN-VMWARE\SQLEXPRESS;Initial Catalog=simplehr;Persist Security Info=True;User ID=sa;Password=12345
+            //
+            string connString = @"Data Source=DESKTOP-LSEGFR1\SQLEXPRESS;Initial Catalog=QUAN_LY_NHAN_SU;Integrated Security=True";
+
+            SqlConnection conn = new SqlConnection(connString);
+            conn.Open();
+            return conn;
+        }
+
+       /* private void getData(string tableName)
+        {
+            SqlConnection conn = GetDBConnection();
+            MySqlDataReader row;
+            row = conn.ExecuteReader(query);
+
+        }*/
+
+
+
+    }
+}
--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient; 
+
 
 namespace WindowsFormsApp1
 {
@@ -17,8 +19,45 @@ namespace WindowsFormsApp1
             InitializeComponent();
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void Form1_Load(object sender, EventArgs e)
         {
+            Console.WriteLine("Getting Connection ...");
+            SqlConnection conn = DBSQLServerUtils.GetDBConnection("DESKTOP-LSEGFR1\\SQLEXPRESS", "QUAN_LY_NHAN_SU");
+
+            SqlCommand sqlCommand;
+            SqlDataReader dataReader;
+            String sql; 
+            sql = "select * from NHANVIEN";
+            sqlCommand = new SqlCommand(sql, conn);
+
+            SqlDataAdapter adapter = new SqlDataAdapter(sql, conn);
+
+
+            DataSet customers = new DataSet();
+            adapter.Fill(customers, "Customers");
+
+            sqlCommand.Dispose();
+            conn.Close(); 
+
+            
+        }
+
+
+        private void readByRow(SqlCommand  sqlCommand)
+        {
+            SqlDataReader dataReader = sqlCommand.ExecuteReader();
+          
+            while (dataReader.Read())
+            {
+                for (int i = 0; i < dataReader.FieldCount; i++)
+                {
+                    Console.WriteLine(dataReader.GetValue(i) + "  ");
+
+
+                }
+                Console.WriteLine("=================");
+            }; 
+
 
         }
     }
